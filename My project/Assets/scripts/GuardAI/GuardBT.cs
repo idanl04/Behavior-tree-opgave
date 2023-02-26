2835b4564d4b6a887a2879ed25de47dcc5584fc3
idanl04 <idanygaardlarsen@gmail.com>
2023-02-26 18:39:29 +0100

    idk

---
+++ My project/Assets/scripts/GuardAI/GuardBT.cs
@@ -0,0 +1,32 @@
+
+using System.Collections.Generic;
+using BehaviorTree;
+
+public class GuardBT : Tree
+{
+    public UnityEngine.Transform[] waypoints;
+
+    public static float speed = 10f;
+    public static float fovRange = 6f;
+    public static float attackRange = 1f;
+
+    protected override Node SetupTree()
+    {
+        Node root = new Selector(new List<Node>
+        {
+            new Sequence(new List<Node>
+            {
+            new CheckEnemyInAttackRange(transform),
+            new TaskAttack(transform),
+            }),
+            new Sequence(new List<Node>
+            {
+                new CheckEnemyInFOVRange(transform),
+                new TaskGoToTarget(transform),
+            }),
+            new TaskPatrol(transform, waypoints),
+        });
+
+        return root;
+    }
+}
